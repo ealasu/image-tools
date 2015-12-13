11b0683f4ca0ce8de3fb9a66f1243bcf05a2b40b
eman <eman>
2015-12-12 17:57:30 -0700

    c

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod star_finder;
 
 fn main() {
     let image = image::Image::load("data/a.gray.tif");
-    let f = star_finder::StarFinder::new(image);
+    let f = star_finder::StarFinder::new(&image);
     let stars = f.find();
     println!("stars: {:?}", stars);
 }
--- src/star_finder.rs
+++ src/star_finder.rs
@@ -6,25 +6,38 @@ pub struct Point {
     pub y: u16,
 }
 
-pub struct StarFinder {
-    image: Image
+pub struct StarFinder<'a> {
+    image: &'a Image
 }
 
-impl StarFinder {
-    pub fn new(image: Image) -> StarFinder {
+impl<'a> StarFinder<'a> {
+    pub fn new(image: &'a Image) -> StarFinder {
         StarFinder {
             image: image
         }
     }
 
     pub fn find(&self) -> Vec<Point> {
-        let im = &self.image;
-        println!("max: {}", im.pixels().iter().max().unwrap());
-        println!("min: {}", im.pixels().iter().min().unwrap());
+        let im = self.image;
+        let max = *im.pixels().iter().max().unwrap() as f32;
+        let min = *im.pixels().iter().min().unwrap() as f32;
+        println!("max: {}", max);
+        println!("min: {}", min);
 
-        let average: f64 = im.pixels().iter().map(|&v| v as f64).fold(0f64, |sum, i| sum + i) /
-            im.pixels().len() as f64;
+        let average: f32 = im.pixels().iter().map(|&v| v as f32).fold(0f32, |sum, i| sum + i) /
+            im.pixels().len() as f32;
         println!("average: {}", average);
+        let background = average;
+
+        let threshold = ((max - background) * 0.75 + background) as u16;
+        let mut c: u32 = 0;
+
+        for (i, &v) in im.pixels().iter().enumerate() {
+            if v >= threshold {
+                c += 1;
+            }
+        }
+        println!("c: {}", c);
 
         Vec::new()
     }
