a607732be86b07252aaea9007b63ce485b4f569b
eman <eman>
2015-12-22 20:14:35 -0700

    c

--- src/star_stacker.rs
+++ src/star_stacker.rs
@@ -1,19 +1,43 @@
 use std::cmp::*;
 use std::collections::BTreeMap;
 use image::Image;
-use point::Vector;
+use point::*;
 
 
 struct ImageStack {
     image: Image,
 }
 
-fn resample(image: &Image, x: f32, y: f32) -> f32 {
+pub fn resample(image: &Image, x: f32, y: f32) -> f32 {
     let mut src_val = 0f32;
-    // SW
-    let xf = 0.5f32 - (src_pos.x - floor(src_pos.x));
-    let yf = 1.0f32 - (src_pos.y - floor(src_pos.y));
-    src_val += image.at(src_pos.x, src_pos.y);
+    let dx = x.ceil() - x;
+    let dy = y.ceil() - y;
+    let dxp = 1.0 - dx;
+    let dyp = 1.0 - dy;
+
+    let sw = dx * dyp;
+    let nw = dx * dy;
+    let ne = dxp * dy;
+    let se = dxp * dyp;
+
+    let e_x = x.ceil() as isize;
+    let s_y = y.ceil() as isize;
+    let w_x = e_x - 1;
+    let n_y = s_y - 1;
+
+    if n_y >= 0 && w_x >= 0 {
+        src_val += image.at(w_x as usize, n_y as usize) * nw;
+    }
+    if n_y >= 0 && e_x < image.width as isize {
+        src_val += image.at(e_x as usize, n_y as usize) * ne;
+    }
+    if s_y < image.height as isize && e_x < image.width as isize {
+        src_val += image.at(e_x as usize, s_y as usize) * se;
+    }
+    if s_y < image.height as isize && w_x >= 0 {
+        src_val += image.at(w_x as usize, s_y as usize) * sw;
+    }
+
     src_val
 }
 
@@ -28,7 +52,7 @@ impl ImageStack {
         for y in (0..self.image.height) {
             for x in (0..self.image.width) {
                 let src_pos = Point {x: x as f32, y: y as f32} - transform;
-                self.image.at_mut(x, y) += resample(image, src_pos.x, src_pos.y);
+                *self.image.at_mut(x, y) += resample(image, src_pos.x, src_pos.y);
             }
         }
     }
@@ -61,4 +85,43 @@ pub fn stack(images: &BTreeMap<String, Vector>) -> Image {
         stack.add(&image, tx);
     }
     stack.to_image()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use image::Image;
+
+    #[test]
+    fn test_1() {
+        let image = Image::from_data(3, 3, vec![
+            0.5, 0.5, 0.5,
+            0.5, 1.0, 0.5,
+            0.5, 0.5, 0.5,
+        ]);
+        let v = resample(&image, 1.0, 1.0);
+        assert_eq!(v, 1.0);
+    }
+
+    #[test]
+    fn test_2() {
+        let image = Image::from_data(3, 3, vec![
+            0.5, 0.5, 0.5,
+            0.5, 1.0, 0.5,
+            0.5, 0.5, 0.5,
+        ]);
+        let v = resample(&image, 0.75, 0.75);
+        assert_eq!(v, (0.75 * 0.75 * 1.0) + (0.75 * 0.25 * 2.0 * 0.5) + (0.25 * 0.25 * 0.5));
+    }
+
+    #[test]
+    fn test_edge() {
+        let image = Image::from_data(3, 3, vec![
+            0.5, 0.5, 0.5,
+            0.5, 1.0, 0.5,
+            0.5, 0.5, 0.5,
+        ]);
+        let v = resample(&image, -0.75, -0.75);
+        assert_eq!(v, 0.25 * 0.25 * 0.5);
+    }
 }
