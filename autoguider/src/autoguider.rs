ad8a47b281f412b8092c7b3eb704b77885b59f47
eman <eman>
2016-12-11 15:12:04 -0700

    use log instead of println

--- autoguider/src/autoguider.rs
+++ autoguider/src/autoguider.rs
@@ -6,9 +6,6 @@ use std::fmt::Debug;
 use crossbeam;
 use signal::{Signal, IterableSignal};
 
-//pub type Image = u64;
-//pub type Pos = u64;
-
 //fn shoot(&self) -> Image;
 //fn correct(&self, Pos);
 //fn calculate_correction(&self, image: Image) -> Pos;
@@ -28,45 +25,29 @@ where Image: Send + Debug, Pos: Send,
       CalcFn: FnMut(Image) -> Pos, CalcFn: Sync + Send,
       CorrectFn: FnMut(Pos), CorrectFn: Send
 {
-    let image_signal = Signal::new();
-    //let correction_signal = Signal::new();
     let camera_mutex = Mutex::new(());
-
-
-    let (image_tx, image_rx) = channel();
-    thread::spawn(move|| {
-        tx.send(10).unwrap();
-    });
-    assert_eq!(rx.recv().unwrap(), 10);Run
-
+    let image_signal = Signal::new();
 
     crossbeam::scope(|scope| {
         scope.spawn(|| {
-            println!("start of thread");
-            'outer loop {
-                for i in image_rx.try_iter() {
-                    let i = if let Some(i) = i {
-                        i
-                    } else {
-                        break 'outer;
-                    };
-                    println!("skipping image {:?}", i);
+            trace!("start of thread");
+            'outer: loop {
+                if let Some(None) = image_signal.get() {
+                    break 'outer;
                 }
-                let image = if let Some(image) = image_rx.recv().unwrap() {
+                let image = if let Some(image) = image_signal.get_wait() {
                     image
                 } else {
                     break 'outer;
                 };
-            for image in image_signal.iter() {
-                println!("thread got image {:?}", image);
+                trace!("thread got image {:?}", image);
                 let correction = calculate_correction(image);
-                println!("thread waiting for correction");
+                trace!("thread locking camera");
                 let lock = camera_mutex.lock().unwrap();
                 correct(correction);
-                //correction_signal.set_wait(correction);
-                println!("thread moving on");
+                trace!("thread moving on");
             }
-            println!("end of thread");
+            trace!("end of thread");
         });
         loop {
             let image = if let Some(image) = {
@@ -77,28 +58,25 @@ where Image: Send + Debug, Pos: Send,
             } else {
                 break
             };
-            println!("got image: {:?}", image);
+            trace!("got image: {:?}", image);
             image_signal.set_notify(Some(image));
-            //let correction = correction_signal.get_notify();
-            //if let Some(correction) = correction {
-                //correct(correction);
-            //}
         }
-        //correction_signal.get_notify();
-        println!("notifying image_signal to end thread");
+        trace!("sending None to end thread");
         image_signal.set_notify(None);
-        println!("end of scope");
+        trace!("end of scope");
     });
-    println!("after scope");
+    trace!("after scope");
 }
 
 #[cfg(test)]
 mod tests {
+    use env_logger;
     use crossbeam;
     use super::*;
 
     #[test]
     fn test() {
+        env_logger::init().unwrap();
         let images = ["1", "2", "3", "4", "5"];
 
         crossbeam::scope(|scope| {
@@ -107,16 +85,16 @@ mod tests {
                 run_autoguider(
                     || {
                         let i = image_iter.next();
-                        println!("shooting {:?}", i);
+                        trace!("shooting {:?}", i);
                         thread::sleep(Duration::from_secs(1));
                         i
                     },
                     |image| {
-                        println!("calculating correction for {:?}", image);
+                        trace!("calculating correction for {:?}", image);
                         image
                     },
                     |pos| {
-                        println!("correcting {:?}", pos);
+                        trace!("correcting {:?}", pos);
                     },
                 );
             });
--- autoguider/src/main.rs
+++ autoguider/src/main.rs
@@ -1,10 +1,14 @@
 #![feature(loop_break_value)]
+#![feature(receiver_try_iter)]
 
+#[macro_use] extern crate log;
+extern crate env_logger;
 extern crate crossbeam;
 
 mod signal;
 mod autoguider;
 
 fn main() {
+    env_logger::init().unwrap();
     println!("Hello, world!");
 }
