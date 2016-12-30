eb3a336bb3600dd81d7dcbda2456475946cb27ce
eman <eman>
2016-12-29 22:49:54 -0700

    clean up

--- image/src/image.rs
+++ image/src/image.rs
@@ -1,18 +1,145 @@
-use channel::*;
+use magick::*;
+use pgm;
+use dcraw;
+use std::default::Default;
+use std::f32;
+use std::fmt;
+use std::iter::repeat;
+use convert::convert_vec;
 
-
-pub trait Image<P> {
-    fn new(width: usize, height: usize) -> Self;
-    fn width(&self) -> usize;
-    fn height(&self) -> usize;
-    fn channels<'a>(&'a self) -> Box<Iterator<Item=&'a Channel<P>> + 'a>;
-    fn channels_mut<'a>(&'a mut self) -> Box<Iterator<Item=&'a mut Channel<P>> + 'a>;
+#[derive(Clone)]
+pub struct Image<P> {
+    pub width: usize,
+    pub height: usize,
+    pub pixels: Vec<P>,
 }
 
-//pub trait RescaleTo<From,To>: Image<From> {
-    //fn rescale_to<I: Image<To>>(&self) -> I;
-//}
+impl<P> fmt::Debug for Image<P> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[image {}x{}]", self.width, self.height)
+    }
+}
 
-//impl RescaleTo<u16, f32> for Image<u16> {
-    //fn rescale_to<I: Image<u32>>(&self) -> I;
-//}
+impl<P> Image<P> {
+    #[inline(always)]
+    pub fn pixel_at(&self, x: usize, y: usize) -> &P {
+        //assert!(x < self.width);
+        //assert!(y < self.height);
+        &self.pixels[x + y * self.width]
+    }
+
+    #[inline(always)]
+    pub fn pixel_at_mut(&mut self, x: usize, y: usize) -> &mut P {
+        //assert!(x < self.width);
+        //assert!(y < self.height);
+        &mut self.pixels[x + y * self.width]
+    }
+}
+
+impl Image<f32> {
+    pub fn open(path: &str) -> Self {
+        let (width, height, data) = magick_stream(path, "gray");
+        Image {
+            width: width,
+            height: height,
+            pixels: data,
+        }
+    }
+
+    pub fn open_pgm(path: &str) -> Self {
+        let (width, height, data) = pgm::read_from_file(path);
+        let data = if let pgm::Format::F32(d) = data { d } else {
+            panic!("bad format")
+        };
+        Image {
+            width: width,
+            height: height,
+            pixels: data,
+        }
+    }
+
+    pub fn save(&self, path: &str) {
+        magick_convert(&self.pixels[..], self.width, self.height, "gray", "grayscale", path);
+    }
+
+    pub fn save_pgm(&self, path: &str) {
+        let data = pgm::Format::F32(self.pixels.clone());
+        pgm::write_to_file(path, self.width, self.height, data);
+    }
+
+    pub fn average(&self) -> f32 {
+        self.pixels.iter().fold(0.0, |acc, v| acc + v) / self.pixels.len() as f32
+    }
+
+    pub fn min(&self) -> f32 {
+        self.pixels.iter().fold(f32::MAX, |acc, &v| acc.min(v))
+    }
+
+    pub fn max(&self) -> f32 {
+        self.pixels.iter().fold(f32::MIN, |acc, &v| acc.max(v))
+    }
+}
+
+impl Image<u16> {
+    pub fn open_raw(path: &str) -> Self {
+        let (width, height, data) = dcraw::open_raw(path);
+        Image {
+            width: width,
+            height: height,
+            pixels: data,
+        }
+    }
+
+    pub fn rescale_to_f32(&self) -> Image<f32> {
+        // TODO: rescale to 0..1?
+        let mut out: Vec<f32> = Vec::with_capacity(self.pixels.len());
+        for v in self.pixels.iter() {
+            out.push(*v as f32);
+        }
+        Image {
+            width: self.width,
+            height: self.height,
+            pixels: out,
+        }
+    }
+}
+
+impl<P: Copy + Clone + Default> Image<P> {
+    fn new(width: usize, height: usize) -> Self {
+        let mut pixels = Vec::with_capacity(width * height);
+        let zero: P = Default::default();
+        pixels.extend(repeat(zero).take(width * height));
+        Image {
+            width: width,
+            height: height,
+            pixels: pixels,
+        }
+    }
+}
+
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct Rgb<T> {
+    r: T,
+    g: T,
+    b: T,
+}
+
+impl Image<Rgb<f32>> {
+    pub fn open(path: &str) -> Self {
+        let (width, height, data) = magick_stream(path, "rgb");
+        let pixels = convert_vec(data);
+        assert_eq!(pixels.len(), width * height);
+        Image {
+            width: width,
+            height: height,
+            pixels: pixels,
+        }
+    }
+
+    pub fn save(&self, path: &str) {
+        let data = convert_vec(self.pixels.clone());
+        magick_convert(&data, self.width, self.height, "rgb", "truecolor", path);
+    }
+}
--- image/src/lib.rs
+++ image/src/lib.rs
@@ -2,17 +2,13 @@
 extern crate convert;
 extern crate regex;
 
-//mod channel;
-//mod image;
 mod magick;
-mod gray_image;
+mod image;
 //mod rgb_image;
 mod pgm;
 mod dcraw;
 mod identify;
 
-//pub use channel::*;
-//pub use image::*;
-pub use gray_image::*;
+pub use image::Image;
 //pub use rgb_image::*;
 pub use identify::*;
