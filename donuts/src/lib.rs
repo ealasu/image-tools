ea422919e0f4ad7fdb66cc09000c77d561d6a7da
eman <eman>
2016-12-29 22:18:00 -0700

    bench

--- donuts/src/lib.rs
+++ donuts/src/lib.rs
@@ -32,3 +32,57 @@ pub fn align(reference: &Projection, sample: &Projection) -> (f32, f32) {
     let y = correlation::calc_offset(&reference.y[..], &sample.y[..], n);
     (x, y)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+    use rand::{self, Rng};
+    use image::GrayImage;
+
+    #[test]
+    fn test_end_to_end() {
+        let ref_image = GrayImage::open("test/ref.jpg");
+        let sample_image = GrayImage::open("test/sample.jpg");
+        let ref_p = preprocess_image(ref_image);
+        let sample_p = preprocess_image(sample_image);
+        let offset = align(&ref_p, &sample_p);
+        assert_eq!(offset, (-15.863456, -12.371683));
+    }
+
+    #[bench]
+    fn bench_preprocess(b: &mut Bencher) {
+        let w = 900;
+        let h = 900;
+        let image = GrayImage {
+            width: w,
+            height: h,
+            pixels: rand::thread_rng().gen_iter().take(w * h).collect()
+        };
+        b.iter(|| {
+            let mut image = image.clone();
+            preprocess_image(image)
+        });
+    }
+
+    #[bench]
+    fn bench_align(b: &mut Bencher) {
+        let w = 900;
+        let h = 900;
+        let ref_image = GrayImage {
+            width: w,
+            height: h,
+            pixels: rand::thread_rng().gen_iter().take(w * h).collect()
+        };
+        let sample_image = GrayImage {
+            width: w,
+            height: h,
+            pixels: rand::thread_rng().gen_iter().take(w * h).collect()
+        };
+        let ref_p = preprocess_image(ref_image);
+        let sample_p = preprocess_image(sample_image);
+        b.iter(|| {
+            align(&ref_p, &sample_p)
+        });
+    }
+}
