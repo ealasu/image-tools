4162ff3ced8579930e4d64c88c5055beb86a093e
eman <eman>
2016-12-29 21:57:20 -0700

    donuts compiles

--- donuts/src/correlation.rs
+++ donuts/src/correlation.rs
@@ -1,3 +1,15 @@
+use std::f32;
+
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+pub fn calc_offset(reference: &[f32], sample: &[f32], n: usize) -> f32 {
+    let corr = correlation(reference, sample, n);
+    correlation_peak(&corr[..])
+}
+
 pub fn correlation(reference: &[f32], sample: &[f32], n: usize) -> Vec<f32> {
     assert_eq!(reference.len(), sample.len());
     let mut res = Vec::with_capacity(n * 2);
@@ -15,6 +27,51 @@ pub fn correlation(reference: &[f32], sample: &[f32], n: usize) -> Vec<f32> {
     res
 }
 
+pub fn correlation_peak(correlation: &[f32]) -> f32 {
+    let peak_pos = pos_of_max(correlation);
+    let n_corr = (correlation.len() - 1) / 2;
+    let peak_offset_estimate = peak_pos as isize - n_corr as isize;
+    let vertex = parabola_vertex(
+        Point {
+            x: (peak_offset_estimate - 1) as f32,
+            y: correlation[peak_pos - 1]
+        },
+        Point {
+            x: peak_offset_estimate as f32,
+            y: correlation[peak_pos]
+        },
+        Point {
+            x: (peak_offset_estimate + 1) as f32,
+            y: correlation[peak_pos + 1]
+        });
+    vertex.x
+}
+
+fn pos_of_max(slice: &[f32]) -> usize {
+    let mut max_pos = 0;
+    let mut max = f32::NEG_INFINITY;
+    for (i, item) in slice.iter().enumerate() {
+        if *item > max {
+            max = *item;
+            max_pos = i;
+        }
+    }
+    max_pos
+}
+
+fn parabola_vertex(p1: Point, p2: Point, p3: Point) -> Point {
+    let denom = (p1.x - p2.x) * (p1.x - p3.x) * (p2.x - p3.x);
+    let a     = (p3.x * (p2.y - p1.y) + p2.x * (p1.y - p3.y) + p1.x * (p3.y - p2.y)) / denom;
+    let b     = (p3.x*p3.x * (p1.y - p2.y) + p2.x*p2.x * (p3.y - p1.y) + p1.x*p1.x * (p2.y - p3.y)) / denom;
+    let c     = (p2.x * p3.x * (p2.x - p3.x) * p1.y + p3.x * p1.x * (p3.x - p1.x) * p2.y + p1.x * p2.x * (p1.x - p2.x) * p3.y) / denom;
+
+    Point {
+        x: -b / (2.0 * a),
+        y: c - b * b / (4.0 * a),
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -22,11 +79,32 @@ mod tests {
     use rand::{self, Rng};
 
     #[test]
-    fn test() {
+    fn test_correlation() {
         let reference = [0.0, 1.0, 2.0, 3.0, 2.0, 1.0, 0.0, 0.0];
         let sample =    [1.0, 0.0, 1.0, 2.0, 4.0, 2.0, 1.0, 0.0];
         let res = correlation(&reference[..], &sample[..], 3);
         let expected = [11.0, 18.0, 22.0, 18.0, 12.0, 6.0, 4.0];
         assert_eq!(res[..], expected[..]);
+    }
+
+    #[test]
+    fn test_zero() {
+        let corr = [0.0, 1.0, 2.0, 1.0, 0.0];
+        let x = correlation_peak(&corr[..]);
+        assert_eq!(x, 0.0);
+    }
+
+    #[test]
+    fn test_one() {
+        let corr = [1.0, 2.0, 1.0, 0.0, 1.0];
+        let x = correlation_peak(&corr[..]);
+        assert_eq!(x, -1.0);
+    }
+
+    #[test]
+    fn test_fraction() {
+        let corr = [1.0, 2.0, 2.0, 1.0, 0.0];
+        let x = correlation_peak(&corr[..]);
+        assert_eq!(x, -0.5);
     }
 }
--- donuts/src/lib.rs
+++ donuts/src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(conservative_impl_trait)]
 #![feature(test)]
 #![feature(iter_max_by)]
 
@@ -8,8 +7,28 @@ extern crate statistical;
 extern crate quickersort;
 extern crate image;
 
-//pub mod image;
 mod remove_background;
 mod projection;
 mod correlation;
-mod align;
+
+use image::GrayImage;
+
+pub struct Projection {
+    x: Vec<f32>,
+    y: Vec<f32>,
+}
+
+pub fn preprocess_image(mut image: GrayImage<f32>) -> Projection {
+    remove_background::remove_background(&mut image, 32);
+    Projection {
+        x: projection::x_projection(&image),
+        y: projection::y_projection(&image),
+    }
+}
+
+pub fn align(reference: &Projection, sample: &Projection) -> (f32, f32) {
+    let n = 100;
+    let x = correlation::calc_offset(&reference.x[..], &sample.x[..], n);
+    let y = correlation::calc_offset(&reference.y[..], &sample.y[..], n);
+    (x, y)
+}
