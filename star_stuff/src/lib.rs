2b96307eb9185bcbaf6ab0f0ec2fd286479e3c5a
eman <eman>
2016-12-31 16:45:10 -0700

    c

--- star_stuff/src/lib.rs
+++ star_stuff/src/lib.rs
@@ -1,22 +1,11 @@
 #![feature(test)]
 
-extern crate test;
-extern crate regex;
-extern crate rayon;
-extern crate itertools;
-extern crate convert;
 extern crate image;
+#[cfg(test)] extern crate test;
 
-#[macro_use]
 pub mod point;
-//pub mod star_finder;
-//pub mod refine_center;
-//pub mod star_aligner;
 pub mod star_stacker;
 pub mod types;
 pub mod math;
-//pub mod triangle;
 
-//pub use star_finder::find_stars;
-//pub use star_aligner::align_images;
 pub use star_stacker::ImageStack;
