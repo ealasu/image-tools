8028274f79718ece29da19bdc052115475b37deb
eman <eman>
2017-01-07 22:39:06 -0700

    c

--- flat/src/main.rs
+++ flat/src/main.rs
@@ -41,41 +41,18 @@ fn main() {
     println!("max: {}", img.max());
     println!("avg: {}", img.average());
 
-    let mut r_max = 0.0;
-    let mut r_count = 0;
-    let mut g_max = 0.0;
-    let mut g_count = 0;
-    let mut b_max = 0.0;
-    let mut b_count = 0;
-    let img = img.to_rggb();
-    for p in img.pixels.iter() {
-        if p.rc > 0.0 && p.r > r_max {
-            r_max += p.r;
-            r_count += 1;
-        }
-        if p.gc > 0.0 && p.g > g_max {
-            g_max += p.g;
-            g_count += 1;
-        }
-        if p.bc > 0.0 && p.b > b_max {
-            b_max += p.b;
-            b_count += 1;
-        }
-    }
-    r_max = r_max / r_count as f32;
-    g_max = g_max / g_count as f32;
-    b_max = b_max / b_count as f32;
-    println!("r_max: {}", r_max);
-    println!("g_max: {}", g_max);
-    println!("b_max: {}", b_max);
+    let (r_avg, g_avg, b_avg) = img.avg();
+    println!("r_avg: {}", r_avg);
+    println!("g_avg: {}", g_avg);
+    println!("b_avg: {}", b_avg);
 
     let img = img.map(|p| {
         if p.rc > 0.0 {
-            p.r / r_max
+            p.r / r_avg
         } else if p.gc > 0.0 {
-            p.g / g_max
+            p.g / g_avg
         } else if p.bc > 0.0 {
-            p.b / b_max
+            p.b / b_avg
         } else {
             unreachable!()
         }
@@ -103,22 +80,22 @@ mod tests {
 
         let mut img = Image::<u16>::open_raw("test.cr2").to_f32();
 
-        img.save("before-nc.tif");
+        img.save("before-nc.jpg");
         let img_b = img.to_rggb();
         println!("before avg: {:?}", img_b.avg());
         let im2 = img_b.correct_white_balance();
         println!("before c avg: {:?}", im2.avg());
         //println!("before min: {}, max: {}", im2.min(), im2.max());
-        im2.to_rgb().to_gray().save("before.tif");
+        im2.to_rgb().to_gray().save("before.jpg");
 
         img /= flat;
 
-        img.save("after-nc.tif");
+        img.save("after-nc.jpg");
         let img_b = img.to_rggb();
         println!("after avg: {:?}", img_b.avg());
         let im2 = img_b.correct_white_balance();
         println!("after c avg: {:?}", im2.avg());
-        im2.to_rgb().to_gray().save("after.tif");
+        im2.to_rgb().to_gray().save("after.jpg");
         //println!("after min: {}, max: {}", im2.min(), im2.max());
 
         //println!("max: {}", img.max());
