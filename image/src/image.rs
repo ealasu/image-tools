43fbf33633b8dd9d38f3a822d1817b8aed22a208
eman <e@m.an>
2017-12-10 10:49:30 -0700

    c

--- image/src/image.rs
+++ image/src/image.rs
@@ -2,160 +2,219 @@ use std::default::Default;
 use std::fmt;
 use std::iter::repeat;
 use std::ops::*;
+use std::slice;
 use rand::{self, Rng, Rand};
 
-#[derive(Clone)]
-pub struct Image<P> {
+// owned image : Vec<Pixel>
+// slice : &[Pixel]
+// view (pixel iter) : Iterator<Pixel>
+
+#[derive(Copy, Clone, Debug)]
+pub struct ImageDimensions {
     pub width: usize,
     pub height: usize,
-    pub pixels: Vec<P>,
+    pub pitch: usize,
 }
 
-impl<P> fmt::Debug for Image<P> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "[image {}x{}]", self.width, self.height)
-    }
+pub trait DimensionedImage {
+    fn dimensions(&self) -> ImageDimensions;
 }
 
-impl<P> Image<P> {
-    #[inline(always)]
-    pub fn pixel_at(&self, x: usize, y: usize) -> &P {
-        //assert!(x < self.width);
-        //assert!(y < self.height);
-        &self.pixels[x + y * self.width]
-    }
+pub trait ImageIter<'a>: DimensionedImage {
+    type Pixel;
+    type Iterator: Iterator<Item=Self::Pixel> + 'a;
 
-    #[inline(always)]
-    pub fn pixel_at_mut(&mut self, x: usize, y: usize) -> &mut P {
-        //assert!(x < self.width);
-        //assert!(y < self.height);
-        &mut self.pixels[x + y * self.width]
-    }
+    fn pixels_iter(&'a self) -> Self::Iterator;
+}
 
-    #[inline(always)]
-    pub fn row(&self, y: usize) -> &[P] {
-        let i = y * self.width;
-        &self.pixels[i..i + self.width]
-    }
+pub trait ImageIterMut<'a>: DimensionedImage {
+    type Pixel: 'a;
+    type Iterator: Iterator<Item=&'a mut Self::Pixel> + 'a;
 
-    #[inline(always)]
-    pub fn row_slice(&self, y: usize, left: usize, right: usize) -> &[P] {
-        let i = y * self.width;
-        &self.pixels[i + left..i + self.width - right]
-    }
+    fn pixels_iter_mut(&'a self) -> Self::Iterator;
+}
 
-    pub fn map<F,R>(&self, f: F) -> Image<R> where F: FnMut(&P) -> R {
-        let pixels = self.pixels.iter().map(f).collect();
-        Image {
-            width: self.width,
-            height: self.height,
-            pixels: pixels,
-        }
+impl<'a, P> ImageIter<'a> for ImageIterMut<'a, Pixel=P> {
+    type Pixel = P;
+    type Iterator = // TODO
+    fn pixels_iter(&'a self) -> Self::Iterator {
+        self.pixels_iter().map(|p| *p)
     }
 }
 
-impl<P: AddAssign> Add for Image<P> {
-    type Output = Self;
-    fn add(mut self, rhs: Self) -> Self {
-        self += rhs;
-        self
+pub trait ImageSlice<'a>: DimensionedImage {
+    type Pixel;
+
+    fn pixels(&'a self) -> &[Self::Pixel];
+    fn pixels_mut(&'a mut self) -> &mut [Self::Pixel];
+}
+
+impl<'a, P> ImageIter<'a> for ImageSlice<'a, Pixel=P> where P: 'a {
+    type Pixel = &'a P;
+    type Iterator = slice::Iter<'a, P>;
+
+    fn pixels_iter(&'a self) -> Self::Iterator {
+        self.pixels().iter()
     }
 }
 
-impl<P: AddAssign> AddAssign for Image<P> {
-    fn add_assign(&mut self, rhs: Self) {
-        assert_eq!(self.width, rhs.width);
-        assert_eq!(self.height, rhs.height);
-        for (l,r) in self.pixels.iter_mut().zip(rhs.pixels.into_iter()) {
-            l.add_assign(r);
-        }
-    }
-}
 
-impl<P: DivAssign + Copy> Div<P> for Image<P> {
-    type Output = Self;
-    fn div(mut self, rhs: P) -> Self {
-        self /= rhs;
-        self
-    }
-}
 
-impl<P: DivAssign + Copy> DivAssign<P> for Image<P> {
-    fn div_assign(&mut self, rhs: P) {
-        for p in self.pixels.iter_mut() {
-            *p /= rhs;
-        }
-    }
-}
 
-impl<'a, P: DivAssign + Copy> Div<&'a Image<P>> for Image<P> {
-    type Output = Self;
-    fn div(mut self, rhs: &Self) -> Self {
-        self /= rhs;
-        self
-    }
-}
+//pub trait Image {
+    //type Pixel;
 
-impl<'a, P: DivAssign + Copy> DivAssign<&'a Image<P>> for Image<P> {
-    fn div_assign(&mut self, rhs: &Self) {
-        for (left, right) in self.pixels.iter_mut().zip(rhs.pixels.iter()) {
-            *left /= *right;
-        }
-    }
-}
+    //fn width(&self) -> usize;
+    //fn height(&self) -> usize;
+    //fn pitch(&self) -> usize;
+    //fn pixels(&self) -> &[Self::Pixel];
 
-impl<P: Copy + Clone + Default> Image<P> {
-    pub fn new(width: usize, height: usize) -> Self {
-        let mut pixels = Vec::with_capacity(width * height);
-        let zero: P = Default::default();
-        pixels.extend(repeat(zero).take(width * height));
-        Image {
-            width: width,
-            height: height,
-            pixels: pixels,
-        }
-    }
-}
+    //#[inline(always)]
+    //fn pixel_at(&self, x: usize, y: usize) -> &Self::Pixel {
+        ////assert!(x < self.width);
+        ////assert!(y < self.height);
+        //&self.pixels()[x + y * self.pitch()]
+    //}
 
-impl<P: Clone> Image<P> {
-    pub fn crop(&self, x: usize, y: usize, width: usize, height: usize) -> Image<P> {
-        assert!(x + width <= self.width, "x too big: {}", x);
-        assert!(y + height <= self.height);
-        let mut pixels = Vec::with_capacity(width * height);
-        for y in y..y + width {
-            let start = y * self.width + x;
-            let end = start + width;
-            pixels.extend_from_slice(&self.pixels[start..end]);
-        }
-        Image {
-            width: width,
-            height: height,
-            pixels: pixels,
-        }
-    }
+    //#[inline(always)]
+    //fn pixel_at_mut(&mut self, x: usize, y: usize) -> &mut Self::Pixel {
+        ////assert!(x < self.width);
+        ////assert!(y < self.height);
+        //&mut self.pixels()[x + y * self.pitch()]
+    //}
 
-    pub fn center_crop(&self, width: usize, height: usize) -> Image<P> {
-        self.crop((self.width - width) / 2, (self.height - height) / 2, width, height)
-    }
-}
+    //#[inline(always)]
+    //fn row(&self, y: usize) -> &[Self::Pixel] {
+        //let i = y * self.pitch();
+        //&self.pixels()[i..i + self.width()]
+    //}
 
-impl<P: Rand> Image<P> {
-    pub fn random(width: usize, height: usize) -> Self {
-        Image {
-            width: width,
-            height: height,
-            pixels: rand::thread_rng().gen_iter().take(width * height).collect()
-        }
-    }
-}
+    //#[inline(always)]
+    //pub fn row_slice(&self, y: usize, left: usize, right: usize) -> &[P] {
+        //let i = y * self.width;
+        //&self.pixels[i + left..i + self.width - right]
+    //}
 
-impl<P: PartialOrd + Copy> Image<P> {
-    pub fn min(&self) -> P {
-        *self.pixels.iter().min_by(|a,b| a.partial_cmp(b).unwrap()).unwrap()
-    }
+    //pub fn map<F,R>(&self, f: F) -> Image<R> where F: FnMut(&P) -> R {
+        //let pixels = self.pixels.iter().map(f).collect();
+        //Image {
+            //width: self.width,
+            //height: self.height,
+            //pixels: pixels,
+        //}
+    //}
+//}
 
-    pub fn max(&self) -> P {
-        *self.pixels.iter().max_by(|a,b| a.partial_cmp(b).unwrap()).unwrap()
-    }
-}
+//impl fmt::Debug for Image {
+    //fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        //write!(f, "[image {}x{}]", self.width(), self.height())
+    //}
+//}
+
+//}
+
+//impl<P: AddAssign> Add for Image<P> {
+    //type Output = Self;
+    //fn add(mut self, rhs: Self) -> Self {
+        //self += rhs;
+        //self
+    //}
+//}
+
+//impl<P: AddAssign> AddAssign for Image<P> {
+    //fn add_assign(&mut self, rhs: Self) {
+        //assert_eq!(self.width, rhs.width);
+        //assert_eq!(self.height, rhs.height);
+        //for (l,r) in self.pixels.iter_mut().zip(rhs.pixels.into_iter()) {
+            //l.add_assign(r);
+        //}
+    //}
+//}
+
+//impl<P: DivAssign + Copy> Div<P> for Image<P> {
+    //type Output = Self;
+    //fn div(mut self, rhs: P) -> Self {
+        //self /= rhs;
+        //self
+    //}
+//}
+
+//impl<P: DivAssign + Copy> DivAssign<P> for Image<P> {
+    //fn div_assign(&mut self, rhs: P) {
+        //for p in self.pixels.iter_mut() {
+            //*p /= rhs;
+        //}
+    //}
+//}
+
+//impl<'a, P: DivAssign + Copy> Div<&'a Image<P>> for Image<P> {
+    //type Output = Self;
+    //fn div(mut self, rhs: &Self) -> Self {
+        //self /= rhs;
+        //self
+    //}
+//}
+
+//impl<'a, P: DivAssign + Copy> DivAssign<&'a Image<P>> for Image<P> {
+    //fn div_assign(&mut self, rhs: &Self) {
+        //for (left, right) in self.pixels.iter_mut().zip(rhs.pixels.iter()) {
+            //*left /= *right;
+        //}
+    //}
+//}
+
+//impl<P: Copy + Clone + Default> Image<P> {
+    //pub fn new(width: usize, height: usize) -> Self {
+        //let mut pixels = Vec::with_capacity(width * height);
+        //let zero: P = Default::default();
+        //pixels.extend(repeat(zero).take(width * height));
+        //Image {
+            //width: width,
+            //height: height,
+            //pixels: pixels,
+        //}
+    //}
+//}
+
+//impl<P: Clone> Image<P> {
+    //pub fn crop(&self, x: usize, y: usize, width: usize, height: usize) -> Image<P> {
+        //assert!(x + width <= self.width, "x too big: {}", x);
+        //assert!(y + height <= self.height);
+        //let mut pixels = Vec::with_capacity(width * height);
+        //for y in y..y + width {
+            //let start = y * self.width + x;
+            //let end = start + width;
+            //pixels.extend_from_slice(&self.pixels[start..end]);
+        //}
+        //Image {
+            //width: width,
+            //height: height,
+            //pixels: pixels,
+        //}
+    //}
+
+    //pub fn center_crop(&self, width: usize, height: usize) -> Image<P> {
+        //self.crop((self.width - width) / 2, (self.height - height) / 2, width, height)
+    //}
+//}
+
+//impl<P: Rand> Image<P> {
+    //pub fn random(width: usize, height: usize) -> Self {
+        //Image {
+            //width: width,
+            //height: height,
+            //pixels: rand::thread_rng().gen_iter().take(width * height).collect()
+        //}
+    //}
+//}
+
+//impl<P: PartialOrd + Copy> Image<P> {
+    //pub fn min(&self) -> P {
+        //*self.pixels.iter().min_by(|a,b| a.partial_cmp(b).unwrap()).unwrap()
+    //}
+
+    //pub fn max(&self) -> P {
+        //*self.pixels.iter().max_by(|a,b| a.partial_cmp(b).unwrap()).unwrap()
+    //}
+//}
 
