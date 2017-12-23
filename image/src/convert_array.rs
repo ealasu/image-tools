316338aa61b5478a91496a1e9333b58064b801f2
eman <e@m.an>
2017-12-23 11:58:51 -0700

    c

--- image/src/convert_array.rs
+++ image/src/convert_array.rs
@@ -2,36 +2,41 @@ use std::mem;
 use std::slice;
 use num::{Bounded, ToPrimitive, Float, NumCast, Num};
 use ndarray::prelude::*;
-use ndarray::{Data};
+use ndarray::{Data, Ix};
 use ::rgb::Rgb;
 
 pub trait ConvertArray {
     type A;
-    type D: Dimension;
 
-    fn stretch<B>(&self, src_min: Self::A, src_max: Self::A, dst_min: B, dst_max: B) -> Array<B, Self::D>
+    fn stretch<B>(&self, src_min: Self::A, src_max: Self::A, dst_min: B, dst_max: B) -> Array2<B>
         where Self::A: Copy + Float + Bounded + ToPrimitive, B: Num + Bounded + NumCast;
-    fn stretch_to_bounds<B>(&self) -> Array<B, Self::D>
+    fn stretch_to_bounds<B>(&self) -> Array2<B>
         where Self::A: Copy + Float + Bounded + ToPrimitive, B: Num + Bounded + NumCast;
 
-    fn scale_to_f32(&self) -> Array<f32, Self::D>
+    fn scale_to_f32(&self) -> Array2<f32>
+        where Self::A: Copy + Num + Bounded + ToPrimitive;
+    fn scale_to_f64(&self) -> Array2<f64>
         where Self::A: Copy + Num + Bounded + ToPrimitive;
     fn min_max(&self) -> (Self::A, Self::A)
         where Self::A: Copy + Num + Bounded + ToPrimitive + PartialOrd;
 
-    fn to_rgb(&self) -> Array<Rgb<Self::A>, Self::D>
+    fn to_rgb(&self) -> Array2<Rgb<Self::A>>
         where Self::A: Copy;
     fn as_bytes(&self) -> &[u8];
+
+    fn crop(&self, x: usize, y: usize, width: usize, height: usize) -> Array2<Self::A>
+        where Self::A: Copy + Clone;
+
+    fn center_crop(&self, width: usize, height: usize) -> Array2<Self::A>
+        where Self::A: Copy + Clone;
 }
 
-impl<A, S, D> ConvertArray for ArrayBase<S, D>
-where S: Data<Elem = A>,
-      D: Dimension
+impl<A, S> ConvertArray for ArrayBase<S, Dim<[Ix; 2]>>
+where S: Data<Elem = A>
 {
     type A = A;
-    type D = D;
 
-    fn stretch<B>(&self, src_min: Self::A, src_max: Self::A, dst_min: B, dst_max: B) -> Array<B, Self::D>
+    fn stretch<B>(&self, src_min: Self::A, src_max: Self::A, dst_min: B, dst_max: B) -> Array2<B>
     where Self::A: Copy + Float + Bounded + ToPrimitive, B: Num + Bounded + NumCast {
         let dst_d = <Self::A as NumCast>::from(dst_max - dst_min).unwrap();
         let src_d = src_max - src_min;
@@ -46,7 +51,7 @@ where S: Data<Elem = A>,
         })
     }
 
-    fn stretch_to_bounds<B>(&self) -> Array<B, Self::D>
+    fn stretch_to_bounds<B>(&self) -> Array2<B>
     where Self::A: Copy + Float + Bounded + ToPrimitive, B: Num + Bounded + NumCast {
         let dst_min = <B as Bounded>::min_value();
         let dst_max = <B as Bounded>::max_value();
@@ -54,11 +59,18 @@ where S: Data<Elem = A>,
         self.stretch(src_min, src_max, dst_min, dst_max)
     }
 
-    fn scale_to_f32(&self) -> Array<f32, Self::D>
+    fn scale_to_f32(&self) -> Array2<f32>
     where Self::A: Copy + Num + Bounded + ToPrimitive {
         let src_max = <Self::A as Bounded>::max_value().to_f32().unwrap();
         let dst_max = <f32 as Bounded>::max_value();
         self.mapv(|p| p.to_f32().unwrap_or(dst_max) / src_max)
+    }
+
+    fn scale_to_f64(&self) -> Array2<f64>
+    where Self::A: Copy + Num + Bounded + ToPrimitive {
+        let src_max = <Self::A as Bounded>::max_value().to_f64().unwrap();
+        let dst_max = <f64 as Bounded>::max_value();
+        self.mapv(|p| p.to_f64().unwrap_or(dst_max) / src_max)
     }
 
     fn min_max(&self) -> (Self::A, Self::A)
@@ -76,7 +88,7 @@ where S: Data<Elem = A>,
         (min, max)
     }
 
-    fn to_rgb(&self) -> Array<Rgb<Self::A>, Self::D>
+    fn to_rgb(&self) -> Array2<Rgb<Self::A>>
     where Self::A: Copy {
         self.mapv(|p| Rgb { r: p, g: p, b: p })
     }
@@ -89,5 +101,24 @@ where S: Data<Elem = A>,
                 s.as_ptr() as *const u8,
                 s.len() * mem::size_of::<Self::A>())
         }
+    }
+
+    fn crop(&self, x: usize, y: usize, width: usize, height: usize) -> Array2<Self::A>
+    where Self::A: Copy + Clone {
+        assert!(x + width <= self.shape()[1], "x too big: {}", x);
+        assert!(y + height <= self.shape()[0]);
+        let slice = self.view().into_slice().unwrap();
+        let mut pixels = Vec::with_capacity(width * height);
+        for y in y..y + width {
+            let start = y * self.shape()[1] + x;
+            let end = start + width;
+            pixels.extend_from_slice(&slice[start..end]);
+        }
+        Array2::from_shape_vec((height, width), pixels).unwrap()
+    }
+
+    fn center_crop(&self, width: usize, height: usize) -> Array2<Self::A>
+    where Self::A: Copy + Clone {
+        self.crop((self.shape()[1] - width) / 2, (self.shape()[0] - height) / 2, width, height)
     }
 }
--- misc-tests/src/lib.rs
+++ misc-tests/src/lib.rs
@@ -5,50 +5,97 @@ extern crate rawspeed;
 extern crate turbojpeg;
 extern crate glob;
 extern crate ndarray;
+extern crate crossbeam_utils;
+extern crate crossbeam_channel;
 
+use std::fs;
+use glob::glob;
+use crossbeam_utils::scoped::scope;
+use crossbeam_channel::{bounded, unbounded};
+use ndarray::prelude::*;
+use image::prelude::*;
+
+pub fn write_jpeg(img: &Array2<f64>, count: usize) {
+    let mut img = img.mapv(|v| {
+        v.powf(1.0 / 3.2)
+    });
+    img /= count as f64;
+    println!("minmax: {:?}", img.min_max());
+    //let img = img.stretch::<u8>(0.0, 0.04, 0, 255).to_rgb();
+    let img = img.stretch_to_bounds::<u8>().to_rgb();
+    fs::write(format!("out/stacked_{:04}.jpg", count), turbojpeg::compress(&img).unwrap()).unwrap();
+}
+
+pub fn stack() {
+    let (in_tx, in_rx) = unbounded();
+    let (out_tx, out_rx) = bounded(1);
+    //let pattern = "/Volumes/data/photos/2017/2017-12-16-darks/101CANON/*.CR2";
+    let pattern = "/Volumes/data/photos/2017/2017-12-16-bias-frames/*.CR2";
+    //let pattern = "test/*.CR2";
+    for entry in glob(pattern).unwrap() {
+        in_tx.send(entry.unwrap()).unwrap();
+    }
+    drop(in_tx);
+    let img = scope(|scope| {
+        for _ in 0..3 {
+            let out_tx = out_tx.clone();
+            let in_rx = in_rx.clone();
+            scope.spawn(move || {
+                for filename in in_rx.iter() {
+                    let img = rawspeed::decode(&fs::read(filename).unwrap()).unwrap()
+                        .center_crop(600, 600)
+                        .scale_to_f64();
+                    out_tx.send(img).unwrap();
+                }
+            });
+        }
+        drop(out_tx);
+        let mut stack = out_rx.recv().unwrap();
+        let mut count: usize = 1;
+        write_jpeg(&stack, count);
+        for img in out_rx.iter() {
+            stack += &img;
+            count += 1;
+            println!("{}", count);
+            write_jpeg(&stack, count);
+        }
+        stack /= count as f64;
+        stack
+    });
+}
 
 #[cfg(test)]
 mod tests {
-    use std::fs;
-    use turbojpeg;
-    use rawspeed;
-    use image::Image;
-    use image::prelude::*;
-    use glob::glob;
-    use ndarray::prelude::*;
+    use super::*;
 
     #[test]
     fn it_works() {
+        stack();
         // 1. read a bias frame, stretch & save as jpeg
         //stretch_raw("data/bias-frames/IMG_1010.CR2", "out/bias.jpg");
         //stretch_raw("test/bias-frame.CR2", "out/bias-frame.jpg", false);
         //stretch_raw("test/dark-frame.CR2", "out/dark-frame.jpg", false);
         //stretch_raw("test/dark-frame.CR2", "out/dark-frame-minus-bias.jpg", true);
         //stretch_raw("test/dark-frame-2.CR2", "out/dark-frame-2.jpg", false);
+        //
 
-        let mut stack = None;
-        let mut count: usize = 0;
-        for entry in glob("/Volumes/data/photos/2017/2017-12-16-darks/101CANON/*.CR2").unwrap() {
-            let entry = entry.unwrap();
-            println!("{}", entry.display());
-            let img = rawspeed::decode(&fs::read(entry).unwrap()).unwrap().scale_to_f32();
-            stack = Some(if let Some(mut stack) = stack {
-                stack += &img;
-                count += 1;
-                stack
-            } else {
-                img
-            })
-        }
-        let mut img = stack.unwrap();
-        img /= count as f32;
-        println!("minmax: {:?}", img.min_max());
-        let img = img.stretch::<u8>(0.0, 0.02, 0, 255).to_rgb();
-        fs::write("out/stacked.jpg", turbojpeg::compress(&img).unwrap()).unwrap();
+        //let mut stack = None;
+        //let mut count: usize = 0;
+        //for entry in glob("/Volumes/data/photos/2017/2017-12-16-darks/101CANON/*.CR2").unwrap() {
+            //let entry = entry.unwrap();
+            //println!("{}", entry.display());
+            //let img = rawspeed::decode(&fs::read(entry).unwrap()).unwrap().scale_to_f32();
+            //stack = Some(if let Some(mut stack) = stack {
+                //stack += &img;
+                //count += 1;
+                //stack
+            //} else {
+                //img
+            //})
+        //}
+        //let mut img = stack.unwrap();
+        //println!("minmax: {:?}", img.min_max());
     }
-
-    //fn stack() {
-    //}
 
     //fn stretch_raw(input: &str, output: &str, sub_bias: bool) {
         //let bias = rawspeed::decode(&fs::read("test/bias-frame.CR2").unwrap()).unwrap()
